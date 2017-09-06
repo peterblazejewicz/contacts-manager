e317f7e22b9fc7230a6493dedcefb4b0902b64d0
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-09-06 23:02:18 +0200

    Crude project scaffold

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+/**
+ * Import the polyfills and vendor files
+ */
+import './polyfills';
+import './vendors';
+
+/**
+ * Import the global styles
+ */
+import './index.scss';
+
+/**
+ * Import angular
+ */
+import * as angular from 'angular';
---
+++ src/vendors.ts
@@ -0,0 +1,3 @@
+import 'angular';
+import '@uirouter/angularjs';
+import 'angular-translate';
