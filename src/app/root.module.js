c73314e12df1ac0640c091fb7849d90ca47822f2
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2018-03-05 00:01:36 +0100

    HTML5 mode

--- src/app/root.module.js
+++ src/app/root.module.js
@@ -1,5 +1,10 @@
 (function(angular) {
   'use strict';
 
-  angular.module('root', ['common', 'components']);
+  angular.module('root', ['ui.router', 'common', 'components']).config([
+    '$locationProvider',
+    function($locationProvider) {
+      $locationProvider.html5Mode(true);
+    },
+  ]);
 })(angular);
